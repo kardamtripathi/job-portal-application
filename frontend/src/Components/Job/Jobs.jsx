f7144079ff3f86ab27355b168acdccffce9b0c3f
kardamtripathi <94391109+kardamtripathi@users.noreply.github.com>
2024-05-06 18:40:01 +0530

    added loader

--- frontend/src/Components/Job/Jobs.jsx
+++ frontend/src/Components/Job/Jobs.jsx
@@ -1,47 +1,61 @@
-import React, { useContext, useEffect, useState } from 'react'
-import {useNavigate, Link} from 'react-router-dom'
-import {Context} from '../../main';
-import axios from 'axios'
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { Context } from '../../main';
+import axios from 'axios';
 import { BACKEND_URL } from '../../BackendUrl';
+import { CircularProgress } from '@mui/material';
+
 const Jobs = () => {
   const [jobs, setJobs] = useState([]);
-  const {isAuthorized} = useContext(Context);
+  const [loading, setLoading] = useState(true);
+  const { isAuthorized } = useContext(Context);
   const navigate = useNavigate();
+
   useEffect(() => {
-    try{
-      axios.get(`${BACKEND_URL}/api/job/getJobs`, {withCredentials: true})
-      .then((res) => {
-        setJobs(res.data)
-      })
-    }
-    catch(error){
-      console.log(error)
+    try {
+      axios.get(`${BACKEND_URL}/api/job/getJobs`, { withCredentials: true })
+        .then((res) => {
+          setJobs(res.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error('Error fetching jobs:', error);
+          setLoading(false);
+        });
+    } catch (error) {
+      console.log(error);
     }
   }, []);
+
   useEffect(() => {
-    if(!isAuthorized){
-      navigate("/login")
+    if (!isAuthorized) {
+      navigate('/login');
     }
-  }, [isAuthorized])
+  }, [isAuthorized]);
+
   return (
     <section className='jobs page'>
       <div className="container">
         <h1>Available Jobs</h1>
-        <div className="banner">
-          {jobs.jobs && jobs.jobs.map((element) => {
-            return (
-              <div className="card" key={element._id}>
-                <p>{element.title}</p>
-                <p>{element.category}</p>
-                <p>{element.country}</p>
-                <Link to={`/job/${element._id}`}>More</Link>
-              </div>
-            )
-          })}
-        </div>
+        {loading ? (
+          <CircularProgress style={{color: "#0096c7"}} />
+        ) : (
+          <div className="banner">
+            {jobs.jobs && jobs.jobs.map((element) => {
+              return (
+                <div className="card" key={element._id}>
+                  <p>{element.title}</p>
+                  <p>{element.category}</p>
+                  <p>{element.country}</p>
+                  <Link to={`/job/${element._id}`}>More</Link>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Jobs
+export default Jobs;
